c41e9e7a185a75455c9036f253e94c64cd34efa5
Isabela Telles <83988818+liratisa@users.noreply.github.com>
2025-12-29 22:40:38 -0300

    Add files via upload

---
+++ Entrega final/relatorios.sql
@@ -0,0 +1,21 @@
+-- Relatórios
+
+-- 1. Total de movimentações feitas por cada responsável
+
+SELECT * FROM vw_movimentacoes;
+
+-- 2. Total de equipamentos, por modelo
+
+SELECT * FROM vw_equipamentospormodelo;
+
+-- 3. Total de equipamentos, por marca
+
+SELECT * FROM vw_equipamentospormarca;
+
+-- 4. Total de servidores, por órgão
+
+SELECT * FROM vw_servidorespororgao;
+
+-- 5. Total de equipamentos disponíveis no parque
+
+SELECT * FROM vw_totalequipamentos;
